9f201c2a25c8a986c8774c2bba2e0f35bc027a01
amr12345a <31047545+amr12345a@users.noreply.github.com>
2021-08-31 13:40:13 +0200

    Add files via upload

---
+++ dodo.py
@@ -0,0 +1,16 @@
+from telethon import TelegramClient, events
+
+
+if __name__ == '__main__':
+    client2 = TelegramClient("asdfrgvrg", 6284107, "376f444df0fcadd27458653b3f92ed6a")
+
+    print("Start")
+
+
+    @client2.on(events.NewMessage(-592668664))
+    async def my_event_handler(event):
+        await client2.forward_messages(-1001571217213, event.message)
+
+
+    client2.start()
+    client2.run_until_disconnected()
