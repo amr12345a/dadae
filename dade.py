cdc6eee88518cef2618f5d69121df016ddfb809d
amr12345a <31047545+amr12345a@users.noreply.github.com>
2021-09-02 18:42:49 +0200

    Update dade.py

--- dade.py
+++ dade.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     @client2.on(events.NewMessage())
     async def my_event_handler(event):
-        await client2.forward_messages(-560266164, event.message)
+        await client2.forward_messages(-1296890742, event.message)
         sender = await event.get_sender()
         print(sender.id)
 
